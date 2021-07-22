6fe45500ecbb82fcccfe1f956538a1b50dd4635c
chloebarber <chloe_barber@outlook.com>
2021-07-22 19:51:03 -0400

    ready for main merge

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -11,7 +11,11 @@ function HomePage(){
             <div className='home-main'>
                 <img src='./images/home_possum2.png' alt="Possum Party Logo"/>
                 <div className='welcome-text'>
-                    <span>Tired of corporate food waste? Want to save on your bills? Join the ever-growing </span>
+                    <div>Tired of corporate food waste?</div>
+                    <div>Want to save on your bills?</div>
+                    <div>Don't mind getting your hands dirty?</div>
+                    <p/>
+                    <span> Join the ever-growing </span>
                     <a href='https://en.wikipedia.org/wiki/Freeganism'>Freeganism</a>
                     <span> movement! Most of the food thrown away by big grocery stores is still perfectly safe and edible, even if it's technically past its sell-by date. Although there is still some stigma surrounding "dumpster diving," informed and safe diving is a great way to combat food waste while saving a few bucks in the process!</span>
                     <h3>Why not find a Dive Spot near you, and join the Possum Party today?</h3>
--- frontend/src/components/SpotDirectory/DiveSpotListing.js
+++ frontend/src/components/SpotDirectory/DiveSpotListing.js
@@ -10,7 +10,61 @@ function DiveSpotListingPage(){
     
     // const selectedSpot = useSelector((state) => state.spots.currSpot);
     const allSpots = useSelector((state) => state.spots.allSpots);
-    
+    const allUSStates = ["Alaska",
+    "Alabama",
+    "Arkansas",
+    "American Samoa",
+    "Arizona",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "District of Columbia",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Guam",
+    "Hawaii",
+    "Iowa",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Massachusetts",
+    "Maryland",
+    "Maine",
+    "Michigan",
+    "Minnesota",
+    "Missouri",
+    "Mississippi",
+    "Montana",
+    "North Carolina",
+    "North Dakota",
+    "Nebraska",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "Nevada",
+    "New York",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Puerto Rico",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Virginia",
+    "Virgin Islands",
+    "Vermont",
+    "Washington",
+    "Wisconsin",
+    "West Virginia",
+    "Wyoming"];
     // const [errors, setErrors] = useState([]);
     
     const dispatch = useDispatch();
@@ -21,11 +75,39 @@ function DiveSpotListingPage(){
         dispatch(cleanupSpot());
     },[dispatch])
     
+    let currIndex = 0;
+
+    // if(allSpots){ //state test code
+    //     return (
+    //     <div className="diveSpotWrapper">
+    //             <h1>All Spots</h1>
+    //         <div className="spotList">
+    //             {allUSStates.map((state)=> {
+    //                 return (
+    //                 <div className='stateDiv'>
+    //                     <h2 className='stateName'>{state}</h2>
+    //                     {allSpots.map((spot) => {
+    //                         return (
+    //                             <li>
+    //                             <Link to={`/divespots/${spot.id}`}>{spot.title}</Link>
+    //                             </li>
+    //                         )
+    //                     })}
+    //                 </div>)
+    //             })}
+    //         </div>
+    //         <Link to="/divespots/new">Found a new dumpster, and wanna share? Click here!</Link>
+    //     </div>
+    //     )
+    // }
+    // else
+    //     return(<h1>Loading...</h1>)
+    
     if(allSpots){
         return (
         <div className="diveSpotWrapper">
-            <div className="spotList">
                 <h1>All Spots:</h1>
+            <div className="spotList">
                 {allSpots.map((spot) => {
                     return (
                         <li>
